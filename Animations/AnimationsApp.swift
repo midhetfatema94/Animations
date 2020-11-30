fec791499716c6c8e6749807d9666fa46421d64e
Midhet Sulemani <midhetfatema94@gmail.com>
2020-11-30 13:16:03 +0300

    first commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Waveline Media on 11/30/20.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
